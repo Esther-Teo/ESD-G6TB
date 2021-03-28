cbdf214fd36e48936e719842ff9cfc3e867fa451
night-skhye <>
2021-03-28 20:56:55 +0800

    Updated tables
    updated tables for tutor+user to be able to edit

--- userMS/user.sql
+++ userMS/user.sql
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 -- phpMyAdmin SQL Dump
 -- version 4.7.4
 -- https://www.phpmyadmin.net/
@@ -110,4 +111,122 @@ COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+=======
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 14, 2019 at 06:42 AM
+-- Server version: 5.7.19
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: user
+--
+DROP DATABASE IF EXISTS user;
+CREATE DATABASE IF NOT EXISTS user DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE user;
+
+-- --------------------------------------------------------
+
+--
+-- 
+--
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
+  userID BIGINT NOT NULL AUTO_INCREMENT,
+  userName VARCHAR(100) NOT NULL,
+  userEmail VARCHAR(100) NOT NULL,
+  pass VARCHAR(20) NOT NULL,
+  userPhone INT NOT NULL,
+  loc VARCHAR(1000) NOT NULL,
+  PRIMARY KEY (userID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 
+--
+
+INSERT INTO users (userName, userEmail, pass, userPhone, loc) VALUES
+( 'Michael Scarn', 'mikescarn@gmail.com', "helps",'12354678', 'Jurong East'),
+('Dwight Snoot', 'dwightsnoot@gmail.com', "gg", '96857412', 'Yishun'),
+('Nard Dog', 'andybernard@gmail.com',"helps", '21325465', 'Sengkang'),
+('Mary Juana', 'maryjuana@gmail.com', "helps",'78459865', 'Tampines'),
+('Jimothy Halpert', 'jimhalpert@gmail.com',"helps", '01472558', 'Woodlands');
+COMMIT;
+
+-- create a new table for child
+DROP TABLE IF EXISTS child;
+CREATE TABLE IF NOT EXISTS child (
+  userID BIGINT NOT NULL,
+  childName VARCHAR(20) NOT NULL,
+  school VARCHAR(100) NOT NULL,
+  pri BOOLEAN NOT NULL,
+  lvl INT NOT NULL,
+  PRIMARY KEY (userID, childName),
+  CONSTRAINT FK_userID FOREIGN KEY (userID)
+    REFERENCES users(userID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- create a table for child's subjects 
+DROP TABLE IF EXISTS childSubjects;
+CREATE TABLE IF NOT EXISTS childSubjects (
+  userID BIGINT NOT NULL,
+  childName VARCHAR(20) NOT NULL,
+  subjects VARCHAR(100) NOT NULL,
+  PRIMARY KEY (userID, childName, subjects),
+  CONSTRAINT FK_userID2 FOREIGN KEY (userID, childName)
+    REFERENCES child(userID, childName)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table child
+--
+
+
+
+INSERT INTO child (userID, childName, school, pri, lvl) VALUES
+('1', 'meep', 'Raffles Primary School', True, '4'),
+('1', 'zaikia', 'Nanyang Girls High School', False, '2'),
+('2', 'oof','Dunman High School ', False, '3'),
+('2', 'lostcause','Rosyth School', True, '3'),
+('3', 'helpsLah','Clementi Primary School', True, '2'),
+('4', 'prettyBaby','Yishun Primary School', True, '5'),
+('5', 'yourmom','Anderson Secondary School', False, '2');
+COMMIT;
+
+
+INSERT INTO childSubjects (userID, childName, subjects) VALUES
+('1', 'meep', 'Mathematics'),
+('1', 'meep', 'English'),
+('1', 'zaikia', 'Additional Mathematics'),
+('1', 'zaikia',  'History'),
+('2', 'oof','Chemistry'),
+('2', 'lostcause', 'Chinese'),
+('3', 'helpsLah', 'Science'),
+('3', 'helpsLah',  'Mathematics'),
+('3', 'helpsLah', 'English'),
+('4', 'prettyBaby', 'Tamil'),
+('4', 'prettyBaby', 'Science'),
+('5', 'yourmom', 'Physics');
+COMMIT;
+
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+>>>>>>> Stashed changes
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
