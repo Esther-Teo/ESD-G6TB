3217e37360ae90260e789991f382d9031948b48e
CrystalFoo <crystal.foo.2018@smu.edu.sg>
2021-04-04 00:31:50 +0800

    Updated invokes.py, manageOffers.py
    -manageOffers.py now processes assignments instead of offers
    -added print statements in invokes.py to better trace errors
    -still experimenting trying to get it to work

--- invokes.py
+++ invokes.py
@@ -17,22 +17,39 @@ def invoke_http(url, method='GET', json=None, **kwargs):
 
     try:
         if method.upper() in SUPPORTED_HTTP_METHODS:
+            print("NEW TRY")
             r = requests.request(method, url, json = json, **kwargs)
+            print("Request is populated")
+            # r = requests.get(method, url, json = json, **kwargs)
+
+            # r = {"code": 200, "offers": "None"}
         else:
             raise Exception("HTTP method {} unsupported.".format(method))
     except Exception as e:
+        print("Requests is empty")
         code = 500
         result = {"code": code, "message": "invocation of service fails: " + url + ". " + str(e)}
     if code not in range(200,300):
+        print("Code not in range 200, 300")
         return result
 
     ## Check http call result
-    if r.status_code != requests.codes.ok:
+    # r.status_code = code
+    print("STATUS CODE:", r.status_code)
+
+    if r.status_code != requests.codes.ok: # requests.codes.ok = 200
         code = r.status_code
+        
     try:
-        result = r.json() if len(r.content)>0 else ""
+        print("R.json:", r.json())
+        print("R.content:", r.content)
+        result = r.json() if len(r.content)>0 else "" 
+        print("Results populated")
     except Exception as e:
+        print("Results is empty")
         code = 500
         result = {"code": code, "message": "Invalid JSON output from service: " + url + ". " + str(e)}
-
+    print("Printing results:", result)
     return result
+
+# invoke_http(offer_URL, method='POST', json=offer)
